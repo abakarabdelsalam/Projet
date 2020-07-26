2b4f1560f41272b76f45af3cfacb2e595c4e0579
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2020-07-26 12:30:17 +0200

    Navigation de deux boutons

--- lib/views/ville/ville.dart
+++ lib/views/ville/ville.dart
@@ -1,5 +1,6 @@
 import 'package:PlningVyage/models/activity.model.dart';
 import 'package:PlningVyage/models/trip.model.dart';
+import 'package:PlningVyage/views/ville/widgets/trip_activity_liste.dart';
 import 'package:flutter/material.dart';
 
 import '../../data/data.dart' as data;
@@ -15,6 +16,7 @@ class Ville extends StatefulWidget {
 
 class _VilleState extends State<Ville> {
   Trip mytrip = Trip(activities: [], ville: 'Paris', date: DateTime.now());
+  int index = 0;
 
   void setDate() {
     showDatePicker(
@@ -28,6 +30,12 @@ class _VilleState extends State<Ville> {
           mytrip.date = newDate;
         });
       }
+    });
+  }
+
+  void switchIndex(newIndex) {
+    setState(() {
+      index = newIndex;
     });
   }
 
@@ -49,23 +57,27 @@ class _VilleState extends State<Ville> {
               setDate: setDate,
             ),
             Expanded(
-              child: ActivityList(
-                activities: widget.activities,
-              ),
-            )
+                child: index == 0
+                    ? ActivityList(
+                        activities: widget.activities,
+                      )
+                    : TripActivityList())
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add_comment),
-          title: Text('Découverte'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add_a_photo),
-          title: Text('Mes activités'),
-        )
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_comment),
+            title: Text('Découverte'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_a_photo),
+            title: Text('Mes activités'),
+          )
+        ],
+        onTap: switchIndex,
+      ),
     );
   }
 }
